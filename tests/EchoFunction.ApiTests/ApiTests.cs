d3bc917e977cc0fc144dd1639b027e5e71eb7a9b
Anton Sizikov <sizikov.anton@gmail.com>
2021-01-01 13:36:31 +0100

    use ITestOutputHelper

--- tests/EchoFunction.ApiTests/ApiTests.cs
+++ tests/EchoFunction.ApiTests/ApiTests.cs
@@ -2,19 +2,28 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace EchoFunction.ApiTests
 {
     public class ApiTests
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ApiTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public async Task FunctionReturnsOk()
         {
             var httpClient = new HttpClient();
             var baseUri = Environment.GetEnvironmentVariable("STAGINGSLOTURI");
             var requestUri = new Uri($"https://{baseUri}/api/Echo");
-            Console.WriteLine($"Calling {requestUri.ToString()}");
+            _testOutputHelper.WriteLine($"Calling {requestUri}");
             var responseMessage = await httpClient.GetAsync(requestUri);
+            _testOutputHelper.WriteLine($"Received {responseMessage.StatusCode} in {responseMessage}");
             Assert.True(responseMessage.IsSuccessStatusCode);
         }
     }
