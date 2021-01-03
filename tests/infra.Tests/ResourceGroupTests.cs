618190dd5a7bcb2f4dae5f959ebf371aa460a4d8
Anton Sizikov <sizikov.anton@gmail.com>
2021-01-03 21:40:06 +0100

    fix unit test, tags are now applied

--- tests/infra.Tests/ResourceGroupTests.cs
+++ tests/infra.Tests/ResourceGroupTests.cs
@@ -7,13 +7,13 @@ namespace infra.Tests
     public class ResourceGroupTests
     {
         [Fact]
-        public async Task ResourceGroupShouldContainNoTags()
+        public async Task ResourceGroupShouldContainTags()
         {
             var resources = await Testing.RunAsync<TrainingStack>();
             foreach (var resource in resources.OfType<ResourceGroup>())
             {
                 var tags = await resource.Tags.GetValueAsync();
-                Assert.True(tags is null);
+                Assert.True(tags != null);
             }
         }
     }
